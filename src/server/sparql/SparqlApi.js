f00996a63c677b80b71d6b5f6a2f089906425168
esikkala <esko.ikkala@aalto.fi>
2022-02-16 15:20:55 +0200

    Remove console.log

--- src/server/sparql/SparqlApi.js
+++ src/server/sparql/SparqlApi.js
@@ -21,7 +21,6 @@ export const runSelectQuery = async ({
   if (useAuth) {
     headers.Authorization = `Basic ${process.env.SPARQL_ENDPOINT_BASIC_AUTH}`
   }
-  console.log(query)
   const q = querystring.stringify({ query })
   try {
     const response = await axios({
