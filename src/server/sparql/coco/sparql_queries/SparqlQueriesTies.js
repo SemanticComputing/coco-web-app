e7df3352eb752e604cd975faccba416380711d47
Petri Leskinen <petri.leskinen@aalto.fi>
2023-05-15 11:52:05 +0300

    Optimize Ties: instance page query

--- src/server/sparql/coco/sparql_queries/SparqlQueriesTies.js
+++ src/server/sparql/coco/sparql_queries/SparqlQueriesTies.js
@@ -6,54 +6,53 @@ export const sahaModel = '"&model=ckcc"'
 //  TODO: add suitable letter properties, e.g. subject etc.
 //  TODO: fix node links, e.g. copy and change url in networkNodesQuery
 export const tiePropertiesInstancePage = `
-?id cocos:actor1 ?ego__id ;
-  cocos:actor2 ?alter__id ;
+?id a cocos:Tie ;
   cocos:num_letters ?numLetters ;
   skos:prefLabel ?prefLabel .
-
-BIND(?ego__id as ?ego__prefLabel)
-BIND(?ego__id AS ?ego__dataProviderUrl)
-
-BIND(?alter__id as ?alter__prefLabel)
-BIND(?alter__id AS ?alter__dataProviderUrl)
-
-OPTIONAL
-{
+{ 
+  ?id cocos:actor1 ?ego__id .
   ?ego__id skos:prefLabel ?prefLabel1__id .
   BIND (?prefLabel1__id as ?prefLabel1__prefLabel)
   BIND(CONCAT("/actors/page/", REPLACE(STR(?ego__id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel1__dataProviderUrl)
-}
 
-OPTIONAL
+  BIND(?ego__id as ?ego__prefLabel)
+  BIND(?ego__id AS ?ego__dataProviderUrl)
+}
+UNION
 {
+  ?id cocos:actor2 ?alter__id .
   ?alter__id skos:prefLabel ?prefLabel2__id .
   BIND (?prefLabel2__id as ?prefLabel2__prefLabel)
   BIND(CONCAT("/actors/page/", REPLACE(STR(?alter__id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel2__dataProviderUrl)
-} 
 
-OPTIONAL
+  BIND(?alter__id as ?alter__prefLabel)
+  BIND(?alter__id AS ?alter__dataProviderUrl)
+} 
+UNION
 {
+  {
+    SELECT DISTINCT ?id ?letter__id ?letter__prefLabel  
+    (CONCAT("/letters/page/", REPLACE(STR(?letter__id), "^.*\\\\/(.+)", "$1")) AS ?letter__dataProviderUrl)
+    WHERE {
+
+      ?letter__id cocos:in_tie ?id ;
+                  skos:prefLabel ?letter__prefLabel . 
+
+      OPTIONAL { ?letter__id crm:P4_has_time-span/crm:P82a_begin_of_the_begin ?letter__timespan }
+
+    } 
+    ORDER BY COALESCE(STR(?letter__timespan), CONCAT("9999", ?letter__prefLabel))
+  }
+}
+UNION
+{
+  ?id cocos:actor1 ?ego__id ; cocos:actor2 ?alter__id .
   [] cocos:actor1|cocos:actor2 ?ego__id ; cocos:actor1|cocos:actor2 ?other__id .
   [] cocos:actor1|cocos:actor2 ?alter__id ; cocos:actor1|cocos:actor2 ?other__id .
   FILTER (?other__id!=?ego__id && ?other__id!=?alter__id)
 
   ?other__id skos:prefLabel ?other__prefLabel .
   BIND(CONCAT("/actors/page/", REPLACE(STR(?other__id), "^.*\\\\/(.+)", "$1")) AS ?other__dataProviderUrl)
-} 
-
-OPTIONAL
-{
-  SELECT DISTINCT ?id ?letter__id ?letter__prefLabel # ?letter__dataProviderUrl  
-    (CONCAT("/letters/page/", REPLACE(STR(?letter__id), "^.*\\\\/(.+)", "$1")) AS ?letter__dataProviderUrl)
-    WHERE {
-      
-    ?letter__id cocos:in_tie ?id ;
-     skos:prefLabel ?letter__prefLabel . 
-
-    OPTIONAL { ?letter__id crm:P4_has_time-span/crm:P82a_begin_of_the_begin ?letter__timespan }
-    
-  } 
-  ORDER BY COALESCE(STR(?letter__timespan), CONCAT("9999", ?letter__prefLabel))
 }
 `
 
