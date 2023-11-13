f60afa8bdc05a3a0a33d3b71c759e66bcc3746b8
Petri Leskinen <petri.leskinen@aalto.fi>
2023-11-13 19:31:02 +0200

    Facet table result order

--- src/server/sparql/coco/sparql_queries/SparqlQueriesFonds.js
+++ src/server/sparql/coco/sparql_queries/SparqlQueriesFonds.js
@@ -31,10 +31,12 @@ export const fondsPropertiesInstancePage = `
   BIND(?id as ?uri__prefLabel)
   BIND(?id as ?uri__dataProviderUrl)
 
-  ?id skos:prefLabel ?prefLabel__id .
-  BIND (?prefLabel__id as ?prefLabel__prefLabel)
-  BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
-
+  {
+    ?id skos:prefLabel ?prefLabel__id .
+    BIND (?prefLabel__id as ?prefLabel__prefLabel)
+    BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+  }
+  UNION
   {
     ?id :number_of_letters ?number_of_letters
   }
