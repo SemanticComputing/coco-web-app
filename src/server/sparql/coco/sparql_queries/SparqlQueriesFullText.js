bbd574d2bd710ffcbf67e08be84af437c04fd798
Petri Leskinen <petri.leskinen@aalto.fi>
2024-11-19 08:02:30 +0200

    Tune full text query

--- src/server/sparql/coco/sparql_queries/SparqlQueriesFullText.js
+++ src/server/sparql/coco/sparql_queries/SparqlQueriesFullText.js
@@ -1,9 +1,10 @@
 export const fullTextSearchProperties = `
 VALUES (?type__id ?type__prefLabel ?pagetype ?value_prop) 
 { 
-  (crm:E21_Person "Person" "/actors" :out_degree)
-  (crm:E74_Group "Group" "/actors" :out_degree)
-  (:Family "Family" "/actors" :out_degree)
+  (:ProvidedActor "Actor" "/actors" :out_degree)
+  #(crm:E21_Person "Person" "/actors" :out_degree)
+  #(crm:E74_Group "Group" "/actors" :out_degree)
+  #(:Family "Family" "/actors" :out_degree)
   (crm:E53_Place "Place" "/places" :number_of_events)
   (:Fonds "Fonds" "/fonds" :number_of_letters)
 }
