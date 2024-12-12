959573f68aca1573e84138d2ffb2c66dc259e86b
Petri Leskinen <petri.leskinen@aalto.fi>
2024-12-12 14:11:00 +0200

    Added facets for sender/recipient class/gender

--- src/server/sparql/coco/sparql_queries/SparqlQueriesPrefixes.js
+++ src/server/sparql/coco/sparql_queries/SparqlQueriesPrefixes.js
@@ -9,7 +9,7 @@ PREFIX portal: <http://ldf.fi/coco/portal/>
 PREFIX provided: <http://ldf.fi/coco/provided/> 
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX sch: <http://schema.org/>
+PREFIX sch: <http://schema.org/> 
 PREFiX skos: <http://www.w3.org/2004/02/skos/core#>
 PREFIX spatial: <http://jena.apache.org/spatial#>
 PREFiX text: <http://jena.apache.org/text#> 
