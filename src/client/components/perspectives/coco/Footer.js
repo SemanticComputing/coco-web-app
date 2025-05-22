48d9ee5d5120f83649248f6202b46c1f1390a42a
Annastiina Ahola <anna.s.ahola@gmail.com>
2025-05-22 11:37:50 +0300

    Add SKS logo to footer

--- src/client/components/perspectives/coco/Footer.js
+++ src/client/components/perspectives/coco/Footer.js
@@ -5,6 +5,7 @@ import Box from '@mui/material/Box'
 import aaltoLogo from '../../../img/logos/Aalto_SCI_EN_13_BLACK_2_cropped.png'
 import hyLogo from '../../../img/logos/university-of-helsinki-logo-transparent-black.png'
 import heldigLogo from '../../../img/logos/heldig-logo-transparent-black.png'
+import sksLogo from '../../../img/logos/sks-logo.png'
 
 /**
  * A component for creating a footer. The logos are imported inside this component.
@@ -98,6 +99,29 @@ const Footer = props => {
           component='img'
           src={heldigLogo}
           alt='Helsinki Centre for Digital Humanities logo'
+          sx={{
+            height: '100%'
+          }}
+        />
+      </Box>
+      <Box
+        component='a'
+        href='https://www.finlit.fi/en/'
+        target='_blank'
+        rel='noopener noreferrer'
+        sx={theme => ({
+          width: 118,
+          height: 30,
+          [theme.breakpoints.up(props.layoutConfig.reducedHeightBreakpoint)]: {
+            width: 130,
+            height: 33
+          }
+        })}
+      >
+        <Box
+          component='img'
+          src={sksLogo}
+          alt='Finnish Literature Society logo'
           sx={{
             height: '100%'
           }}
